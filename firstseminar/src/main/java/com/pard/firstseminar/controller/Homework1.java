24fbf9a14bc204d78165f1c8664131557edacf9d
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2024-03-26 15:55:59 +0900

    Add

--- firstseminar/src/main/java/com/pard/firstseminar/controller/Homework1.java
+++ firstseminar/src/main/java/com/pard/firstseminar/controller/Homework1.java
@@ -1,11 +1,17 @@
 package com.pard.firstseminar.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Homework1 {
 
-    @RequestMapping("/work")
-    public String
+    @RequestMapping("/Introduce")
+    public String MyInfo(@RequestParam(defaultValue = "Guest") String name, @RequestParam(defaultValue = "0") Integer age, @RequestParam(defaultValue = "글로벌리더십") String classroom, @RequestParam(defaultValue = "Null") String hobby) {
+        return "제 이름은 박준현이고요, 나이는 23살, 학부는 전산전자공학부, 취미는 번개맴버 모집입니다." +
+                "제 짝의 이름은 " + name + "고요, 나이는 " + age + "살, 학부는 " + classroom + " 취미는 " + hobby + "입니다.";
+    }
+
 }
+
